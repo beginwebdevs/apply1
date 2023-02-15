18e5f57bed16755e2032c7a98e56d082e23e3a4e
beginwebdevs <125490500+beginwebdevs@users.noreply.github.com>
2023-02-15 23:37:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ connectToMongo();
 
 const app = express()
 require('dotenv').config()
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 9000;
 
 app.use(express.json())
 app.use(cors())
