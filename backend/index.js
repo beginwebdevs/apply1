afbaf7790d0277f672311b21eb39d4a040754e2d
beginwebdevs <125490500+beginwebdevs@users.noreply.github.com>
2023-02-22 12:09:03 +0530

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,11 @@ connectToMongo();
 
 const app = express()
 require('dotenv').config()
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use(cors())
+app.use('/storage', express.static('storage'));
 
 // AVAILABLE ROUTES
 
@@ -23,4 +24,6 @@ if(process.env.NODE_ENV == "production") {
 }
 
 
-app.listen()
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port || process.env.PORT}`)
+})
