a5da7bd0cd5c40f77aa14e3d4cc7bd786d12a221
beginwebdevs <125490500+beginwebdevs@users.noreply.github.com>
2023-04-03 16:14:46 +0530

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const connectToMongo = require('./db');
 const express = require('express')
 var cors = require('cors')
+var bodyParser = require("body-parser")
+const cookieParser = require('cookie-parser');
+
 connectToMongo();
 
 
@@ -9,18 +12,24 @@ const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json({limit: '8mb'}))
+app.use(cookieParser())
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+//app.use(semiauthMiddleware)
 app.use('/', express.static('appp'));
 app.use('/storage', express.static('storage'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // AVAILABLE ROUTES
 
 app.use('/api/universities', require('./routes/universities'))
 app.use('/api/courses', require('./routes/courses'))
+app.use('/api/profile', require('./routes/profileController'));
 app.use('/api/users', require('./routes/users'))
-app.use('/api/auth', require('./routes/auth'))
 app.use('/api/fields', require('./routes/fieldofstudy'));
+app.use('/api/email', require('./routes/verifyMail'))
 
 
 
